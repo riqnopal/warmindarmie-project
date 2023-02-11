f7b6de0adbc671e66e5947b1baf486b4023c0686
Ariq Naufal Fadhlurrahman <fjrdrn@gmail.com>
2023-02-12 01:47:23 +0700

    warmindarmie-project

---
+++ warmindar/js/script.js
@@ -0,0 +1,15 @@
+// Toggle Active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika humberger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
